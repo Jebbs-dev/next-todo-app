8b7aaecc828dfd6871b9f666f5dfaa0fddf755d7
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-22 12:26:47 +0100

    feat: add page layout

---
+++ components/Layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-80 fixed min-h-[60vh] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 mx-auto w-full max-w-3xl rounded-2xl p-2 flex flex-row text-center">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;

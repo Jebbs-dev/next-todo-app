bcf04d5ab08afc2c63cf346943289e42cc0fe5cf
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-22 14:21:41 +0100

    feat: add section for completed tasks

---
+++ components/CompletedList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CompletedList = () => {
+  return (
+    <div className="w-1/2 p-3">
+      <div className="mt-2 mb-10">
+        <h1 className="text-4xl font-semibold text-white">Completed Tasks</h1>
+      </div>
+    </div>
+  );
+};
+
+export default CompletedList;

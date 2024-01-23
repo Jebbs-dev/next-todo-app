3dcf0c6f0646f6e2f5172195fc45079d4606b42a
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-23 20:57:45 +0100

    update button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,14 +4,29 @@ interface ButtonProps {
   disabled: boolean;
   onClick: () => void;
   label: string;
+  authWidth: boolean;
+  authEffect: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ disabled, onClick, label }) => {
+const Button: React.FC<ButtonProps> = ({
+  disabled,
+  onClick,
+  label,
+  authWidth,
+  authEffect,
+}) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className="block w-full rounded-md bg-gray-700 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800"
+      className={`block disabled:opacity-70 disabled:cursor-not-allowed w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition
+      ${
+        authWidth
+          ? "w-full bg-white text-black border-black"
+          : "w-fit bg-gray-700 border-gray-700 text-white"
+      }
+      ${authEffect ? "" : "hover:bg-gray-800 focus-visible:outline-gray-800"}
+      `}
     >
       {label}
     </button>

fab83639036030c4af67a4e58b8d14604782f704
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-23 20:57:55 +0100

    update input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,7 +5,9 @@ interface InputComponents {
   value?: string;
   type?: string;
   name?: string;
-  onChange: () => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
+  authInput?: boolean;
 }
 
 const Input: React.FC<InputComponents> = ({
@@ -13,16 +15,24 @@ const Input: React.FC<InputComponents> = ({
   value,
   type,
   onChange,
+  disabled,
+  authInput,
 }) => {
   return (
     <input
       type={type}
       value={value}
-      // name={name}
+      disabled={disabled}
       onChange={onChange}
       placeholder={placeholder}
       autoComplete="given-name"
-      className="relative pt-72 bottom-5 block w-full mx-auto bg-transparent border-t-0 border-red border-b-[1px] py-2 text-white shadow-sm placeholder:text-gray-100 sm:text-sm sm:leading-6 focus:outline-none"
+      className={` w-full bg-transparent text-white shadow-sm placeholder:text-gray-400
+      ${
+        authInput
+          ? "p-4 text-lg border-2 border-neutral-800 rounded-md outline-none text-white focus:border-sky-500 focus:border-2 transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed"
+          : "relative bottom-5  border-t-0 border-b-[1px]  sm:text-sm sm:leading-6 focus:outline-nonept-72 mx-auto"
+      }
+      `}
     />
   );
 };

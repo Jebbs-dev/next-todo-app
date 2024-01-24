e188cb4b5c3e30865a77244967efdde7dc9d1eef
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-24 23:24:07 +0100

    add close function to modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import Button from "./Button";
 
 interface ModalProps {
   isOpen?: boolean;
   onClose: () => void;
-  onSubmit: () => void;
   title?: string;
   body?: React.ReactElement;
   footer?: React.ReactElement;
@@ -15,16 +14,19 @@ interface ModalProps {
 
 const handleSubmit = () => {};
 
+
+
 const Modal: React.FC<ModalProps> = ({
   isOpen,
   onClose,
-  onSubmit,
   title,
   body,
-  footer,
-  actionLabel,
-  disabled,
 }) => {
+
+  const handleClose = useCallback(() => {
+    onClose();
+  }, [ onClose]);
+
   return (
     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800 bg-opacity-70">
       <div className="relative w-4 lg:w-3/6 my-6 mx-auto lg:max-w-md h-full lg:h-auto">
@@ -35,7 +37,7 @@ const Modal: React.FC<ModalProps> = ({
             <h3 className="text-3xl font-semibold text-white">{title}</h3>
           </div>
           {/* Body */}
-          <div className="relative px-10 pb-10 flex-auto">{body}</div>
+          <div className="relative px-10 pb-10 flex-auto" onClick={handleClose}>{body}</div>
         </div>
       </div>
     </div>

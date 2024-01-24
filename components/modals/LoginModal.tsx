f87dfab80401443ec86b30f53bd5511f5725b405
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-24 23:26:21 +0100

    feat: add signin function to loginmodal

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,53 +1,55 @@
 import React, { useCallback, useState } from "react";
-import Input from "../Input";
 import useLoginModal from "@/hooks/useLoginModal";
 import Modal from "../Modal";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa6";
+import { signIn } from "next-auth/react";
 
 const LoginModal = () => {
   const loginModal = useLoginModal();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const onToggle = useCallback(()=>{
-    if (isLoading) return
-  }, [isLoading])
-
-  const onSubmit = useCallback(async () => {
-    try {
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
+  // const onSubmit = useCallback(async () => {
+  //   try {
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // }, []);
 
   const bodyContent = (
     <div className="flex flex-col gap-4 text-white">
-      <div className="w-full flex items-center justify-center gap-2 p-4 border border-neutral-700 rounded-md">
-      <FcGoogle size={25}/> <p className="text-1xl">Google</p>  
+      <div
+        onClick={async () => {
+          signIn("google");
+          await loginModal.onClose;
+        }}
+        className="w-full flex items-center justify-center gap-2 p-4 border border-neutral-700 rounded-md"
+      >
+        <FcGoogle size={25} /> <p className="text-1xl">Google</p>
       </div>
-      <div className="w-full flex items-center justify-center gap-2 p-4 border border-neutral-700 rounded-md">
-      <FaGithub size={25} color="white"/> <p className="text-1xl">GitHub</p> 
+      <div
+        onClick={() => {
+          signIn("github", { redirect: false });
+          // loginModal.onClose;
+        }}
+        className="w-full flex items-center justify-center gap-2 p-4 border border-neutral-700 rounded-md"
+      >
+        <FaGithub size={25} color="white" /> <p className="text-1xl">GitHub</p>
       </div>
-      
     </div>
-  )
+  );
 
   return (
     <Modal
-    onClose={loginModal.onClose}
-    isOpen={loginModal.isOpen}
-    disabled={isLoading}
-    title="Sign in to your account"
-    onSubmit={onSubmit}
-    body={bodyContent}
+      isOpen={loginModal.isOpen}
+      onClose={loginModal.onClose}
+      disabled={isLoading}
+      title="Sign in to your account"
+      // onSubmit={onSubmit}
+      body={bodyContent}
     />
-    
-    
   );
 };
 

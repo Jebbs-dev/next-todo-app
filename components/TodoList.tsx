d64b31da65042f7771d139178b2515b49ca3ca3c
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-24 23:25:14 +0100

    update section for todo tasks

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,23 +1,33 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Input from "./Input";
-import TaskItem from "./TaskItem";
+import TaskItem from "./component/TaskItem";
 import Button from "./Button";
+import useLoginModal from "@/hooks/useLoginModal";
 
 const TodoList = () => {
+  const loginModal = useLoginModal();
+  const [tasks, setTasks] = useState("");
+
   const handleChange = useCallback(() => {}, []);
 
-  const onClick = ()=> {
-
-  }
+  const onClick = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
 
   return (
     <div className="w-1/2 p-3">
       <div className="mt-2 mb-10">
         <h1 className="text-4xl font-semibold text-white">To do List</h1>
       </div>
-      <TaskItem/>
+      <TaskItem />
       <Input type="text" onChange={handleChange} />
-      <Button label="Add task" onClick={onClick} disabled={false} authWidth={false} authEffect={false}/>
+      <Button
+        label="Add task"
+        onClick={onClick}
+        disabled={false}
+        authWidth={false}
+        authEffect={false}
+      />
     </div>
   );
 };

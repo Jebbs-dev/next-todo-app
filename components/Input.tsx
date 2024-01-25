92c1c02b6667991f2bcec3ef892434aa490bbe81
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-25 23:12:41 +0100

    style: update input styling

--- components/Input.tsx
+++ components/Input.tsx
@@ -26,12 +26,7 @@ const Input: React.FC<InputComponents> = ({
       onChange={onChange}
       placeholder={placeholder}
       autoComplete="given-name"
-      className={` w-full bg-transparent text-white shadow-sm placeholder:text-gray-400
-      ${
-        authInput
-          ? "p-4 text-lg border-2 border-neutral-800 rounded-md outline-none text-white focus:border-sky-500 focus:border-2 transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed"
-          : "relative bottom-5  border-t-0 border-b-[1px]  sm:text-sm sm:leading-6 focus:outline-nonept-72 mx-auto"
-      }
+      className={`relative bottom-5 w-full bg-transparent mt-48 text-white shadow-sm placeholder:text-gray-400 border-t-0 border-b-[1px] sm:text-sm sm:leading-6 focus:outline-nonept-72 mx-auto
       `}
     />
   );

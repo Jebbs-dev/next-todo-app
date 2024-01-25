29f32e2e0ea65517833b35855101d8a578eb6227
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-25 23:19:53 +0100

    feat: add session provider for session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,24 @@
 import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import useLoginModal from "@/hooks/useLoginModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps} }: AppProps) {
+  const loginModal = useLoginModal();
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* <RegisterModal/> */}
+      
+      <SessionProvider session={session}>
+        {loginModal.isOpen && <LoginModal />}
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
   );
 }

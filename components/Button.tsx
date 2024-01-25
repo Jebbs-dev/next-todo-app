9f132f30880f1d765cc7facfe18454ccb53dbe17
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-25 23:12:19 +0100

    update button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,31 +1,31 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 interface ButtonProps {
   disabled: boolean;
   onClick: () => void;
   label: string;
-  authWidth: boolean;
-  authEffect: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
   disabled,
-  onClick,
   label,
-  authWidth,
-  authEffect,
+  onClick
 }) => {
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    
+    onClick();
+
+  }, [onClick, disabled]);
+
   return (
     <button
-      onClick={onClick}
+      onClick={handleSubmit}
       disabled={disabled}
-      className={`block disabled:opacity-70 disabled:cursor-not-allowed w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition
-      ${
-        authWidth
-          ? "w-full bg-white text-black border-black"
-          : "w-fit bg-gray-700 border-gray-700 text-white"
-      }
-      ${authEffect ? "" : "hover:bg-gray-800 focus-visible:outline-gray-800"}
+      className={`block disabled:opacity-70 disabled:cursor-not-allowed w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition bg-gray-700 border-gray-700 text-white hover:bg-gray-800 focus-visible:outline-gray-800
       `}
     >
       {label}

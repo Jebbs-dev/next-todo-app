7e937ca7cac36ef9e41bb4ccb9a4f52b0ad182c9
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-25 23:15:20 +0100

    feat: display name and add signout
    Added feature to display name in header and added signout function

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Button from "./Button";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { signOut } from "next-auth/react";
 
 const Nav = () => {
+  const handleClick = () => {};
 
-  const handleClick = () => {}
+  const { data: fetchedUser } = useCurrentUser();
 
   return (
     <nav className="px-12 py-5 flex justify-between items-center">
       <img src="/vercel.svg" className="h-6" alt="Logo" />
-      <h2 className="text-5xl text-white">Hello,</h2>
-      <span className="px-3 py-1 bg-gray-300 rounded-md">Sign in</span>
+      <h2 className="text-5xl text-white">Hello, {fetchedUser?.name}</h2>
+      <span
+        onClick={() => signOut()}
+        className="px-3 py-1 bg-gray-300 rounded-md"
+      >
+        Sign out
+      </span>
     </nav>
   );
 };

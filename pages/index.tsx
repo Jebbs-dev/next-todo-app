ac9ac1c2e1b8c2fbb5439bdd73139bab2d337618
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-30 23:23:14 +0100

    fetch data from backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import CompletedList from "@/components/CompletedList";
 import TodoList from "@/components/TodoList";
+import useCompletedTasks from "@/hooks/useCompletedTasks";
+import useTaskList from "@/hooks/useTaskList";
 
 
 export default function Home() {
+
+  const {data: todoTask} = useTaskList()
+  const {data: completedTask} = useCompletedTasks()
+
   return (
     <>
       <TodoList/>

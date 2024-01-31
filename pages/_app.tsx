51f42edbc6d898b43a80950d571bd4686b21fa05
Jebbs-dev <ojebiyifulness@gmail.com>
2024-01-31 22:22:26 +0100

    feat: wrap application with task provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,19 +5,25 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import TaskProvider from "@/providers/task-provider";
 
-export default function App({ Component, pageProps: { session, ...pageProps} }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   const loginModal = useLoginModal();
 
   return (
     <>
       {/* <RegisterModal/> */}
-      
+
       <SessionProvider session={session}>
-        {loginModal.isOpen && <LoginModal />}
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <TaskProvider>
+          {loginModal.isOpen && <LoginModal />}
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </TaskProvider>
       </SessionProvider>
     </>
   );

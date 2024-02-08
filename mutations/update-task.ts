3307128288bf8607361ef9a1626b755f6925a1ab
Jebbs-dev <ojebiyifulness@gmail.com>
2024-02-08 19:55:02 +0100

    feat: add mutation for updating task status

---
+++ mutations/update-task.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import axios from "axios";
+
+
+export const useUpdateTaskStatus =()=>{
+
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async(taskId: string) => {
+      const response = await axios.patch(`/api/completed`, {
+        taskId,
+      });
+
+      console.log(response.data);
+      return response.data;
+    },
+    onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey: ["tasks"]});
+    }
+  })
+}

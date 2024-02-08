872a6bf186f7260a3a3a961e617d60e74554cc9d
Jebbs-dev <ojebiyifulness@gmail.com>
2024-02-08 19:48:18 +0100

    update index.tsx
    Moved logic for todo tasks and completed tasks to their respective components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,16 @@
-import Button from "@/components/Button";
-import Input from "@/components/Input";
-import TaskList from "@/components/TaskList";
-import useCompletedTasks from "@/hooks/useCompletedTasks";
-import useLoginModal from "@/hooks/useLoginModal";
-import useTaskList from "@/hooks/useTaskList";
-import { useCallback, useState } from "react";
-import axios from "axios";
-import useCurrentUser from "@/hooks/useCurrentUser";
+import { useTasksQuery } from "@/queries/fetch-task";
+import { useUserQuery } from "@/queries/fetch-user";
+import CompletedList from "@/components/CompletedList";
+import { Task } from "@prisma/client";
+import TodoList from "@/components/TodoList";
 
 export default function Home() {
-  const { data: todoTask = [] } = useTaskList();
-  const { data: completedTask = [] } = useCompletedTasks();
-  const { data: fetchedUser } = useCurrentUser();
-
-
-  const loginModal = useLoginModal();
-
-
-  const [title, setTitle] = useState("");
-
-  const addTask = useCallback(async () => { if (!fetchedUser) {
-    loginModal.onOpen();
-  }
-  try {
-    if (title.length > 0) {
-      await axios.post("/api/task", {
-        title,
-      });
-    }
-  } catch (error) {
-    console.log(error);
-  } finally {
-    setTitle("");
-  }
-}, [fetchedUser, loginModal, title]);;
+  const { data: todoTask, isLoading, isError, error } = useTasksQuery();
 
   return (
     <>
-      <div className="w-1/2 p-3">
-        <TaskList data={todoTask} heading="To do List" />
-        <Input
-          type="text"
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-          value={title}
-        />
-        <Button label="Add task" onClick={addTask} disabled={false} />
-      </div>
-
-      <div className="w-1/2 p-3">
-        <TaskList data={completedTask} heading="Completed Tasks" />
-      </div>
+      <TodoList />
+      <CompletedList tasks={todoTask as Task[]} isLoading={isLoading} />
     </>
   );
 }

3725e0f0d1925d459b0de8720e988fe66a82e141
Jebbs-dev <ojebiyifulness@gmail.com>
2024-02-08 19:52:19 +0100

    style: change margin spacing

--- components/Input.tsx
+++ components/Input.tsx
@@ -26,7 +26,7 @@ const Input: React.FC<InputComponents> = ({
       onChange={onChange}
       placeholder={placeholder}
       autoComplete="given-name"
-      className={`relative bottom-5 w-full bg-transparent mt-48 text-white shadow-sm placeholder:text-gray-400 border-t-0 border-b-[1px] sm:text-sm sm:leading-6 focus:outline-nonept-72 mx-auto
+      className={`relative bottom-5 w-full bg-transparent mt-10 text-white shadow-sm placeholder:text-gray-400 border-t-0 border-b-[1px] sm:text-sm sm:leading-6 focus:outline-nonept-72 mx-auto
       `}
     />
   );

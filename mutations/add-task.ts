0e0023599c2b035a24b3fd4e9cba9f219458708e
Jebbs-dev <ojebiyifulness@gmail.com>
2024-02-08 19:54:30 +0100

    feat: add mutation for adding new task

---
+++ mutations/add-task.ts
@@ -0,0 +1,25 @@
+import { TaskProps } from "@/types/task";
+import { Task } from "@prisma/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import axios from "axios";
+
+
+export const useTaskMutation =()=>{
+
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async(task: TaskProps) => {
+      const response = await axios.post("/api/task", {
+        title: task.title,
+        isCompleted: task.isCompleted,
+        status: task.status,
+      });
+
+      return response.data;
+    },
+    onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey: ["tasks"]});
+    }
+  })
+}

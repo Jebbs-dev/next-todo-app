fcf61199d5a1df0b40c1a4d6434d33be0d4674fc
Jebbs-dev <ojebiyifulness@gmail.com>
2024-02-12 11:19:37 +0100

    feat: conditionally display username on login

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import Button from "./Button";
-import useCurrentUser from "@/hooks/useCurrentUser";
 import { signOut } from "next-auth/react";
+import { useUserQuery } from "@/queries/fetch-user";
 
 const Nav = () => {
   const handleClick = () => {};
 
-  const { data: fetchedUser } = useCurrentUser();
+  const { data: fetchedUser } = useUserQuery();
 
   return (
     <nav className="px-12 py-5 flex justify-between items-center">
       <img src="/vercel.svg" className="h-6" alt="Logo" />
-      <h2 className="text-5xl text-white">Hello, {fetchedUser?.name}</h2>
+      <h2 className="text-5xl text-white">Hello, {fetchedUser?.name ? fetchedUser?.name : `User`}</h2>
       <span
         onClick={() => signOut()}
         className="px-3 py-1 bg-gray-300 rounded-md"

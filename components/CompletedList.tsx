c439bdee83740b401eacf0a7d043357590875988
Jebbs-dev <ojebiyifulness@gmail.com>
2024-02-12 11:15:21 +0100

    feat: add logic to display completed tasks

--- components/CompletedList.tsx
+++ components/CompletedList.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import { Task } from "@prisma/client";
+import React, { useMemo } from "react";
+import toast from "react-hot-toast"
 
-const CompletedList = () => {
+interface CompletedListProps {
+  tasks: Task[]
+  isLoading: boolean
+}
+
+const CompletedList: React.FC<CompletedListProps> = ({ tasks, isLoading }) => {
+
+  const completedTasks = useMemo(() => {
+    
+    return tasks?.filter((task) => task.isCompleted) || null;
+  }, [tasks])
+
+  if(isLoading){
+    // const toastMessage = toast.success("Setting task as completed!")
+    // return toastMessage;
+    return <div>Loading...</div>
+  }
+
   return (
-    <div className="w-1/2 p-3">
-      <div className="mt-2 mb-10">
-        <h1 className="text-4xl font-semibold text-white">Completed Tasks</h1>
-      </div>
+    <div className="w-1/2 px-3">
+      <div className="mt-2 mb-5 top-0 sticky z-20">
+          <h1 className="text-4xl font-semibold text-white">Completed Tasks</h1>
+        </div>
+      {completedTasks?.map((task: Record<string, any>)=>(
+        <p key={task.id} className="text-white ml-5 text-lg text-left p-2">
+          {task?.title}
+      </p>
+      ))}
     </div>
   );
 };

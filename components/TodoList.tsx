73b9aae8e0e4fe26e85fab0054828906352b70d3
Jebbs-dev <ojebiyifulness@gmail.com>
2024-02-12 11:23:56 +0100

    feat: update logic for adding tasks

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,81 +1,91 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 
-import { isEmpty } from "lodash";
 import Input from "./Input";
 import TaskItem from "./component/TaskItem";
 import Button from "./Button";
+
 import useLoginModal from "@/hooks/useLoginModal";
-import axios from "axios";
-import useCurrentUser from "@/hooks/useCurrentUser";
-import useTaskList from "@/hooks/useTaskList";
+import { useTaskMutation } from "@/mutations/add-task";
+import { useTasksQuery } from "@/queries/fetch-task";
+import { useUserQuery } from "@/queries/fetch-user";
+import { Task } from "@prisma/client";
+import { useUpdateTaskStatus } from "@/mutations/update-task";
+import { useDeleteTasks } from "@/mutations/delete-task";
 
+const TodoList = () => {
+  const { data: todoTask, isLoading } = useTasksQuery();
+  const { data: fetchedUser } = useUserQuery();
 
-const TodoList: React.FC = () => {
+  const { mutateAsync: addTaskMutation } = useTaskMutation();
+
+  const { mutateAsync: addToggleCheck } = useUpdateTaskStatus();
+
+  const {mutateAsync: addDeleteTask} = useDeleteTasks();
+
   const loginModal = useLoginModal();
-  const { data: fetchedUser, mutate } = useCurrentUser();
-
-  const { data: fetchedTask = [] } = useTaskList();
-  const { mutate: mutateTasks} = useTaskList();
 
   const [title, setTitle] = useState("");
-  // const [isLoading, setIsLoading] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const [status, setStatus] = useState("unchecked");
+  const [isEditable, setIsEditable] = useState(false)
+  const [updateTitle, setUpdateTitle] = useState("")
 
-  const onClick = useCallback(async () => {
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const addTask = async () => {
     if (!fetchedUser) {
       loginModal.onOpen();
     }
     try {
       if (title.length > 0) {
-        await axios.post("/api/task", {
-          title,
-        });
+        await addTaskMutation({ title, isCompleted, status });
       }
     } catch (error) {
       console.log(error);
     } finally {
       setTitle("");
     }
-  }, [fetchedUser, loginModal, title]);
-
-  const getStatus = (status: string) => {
-    if (status === "unchecked") {
-      return "checked";
-    } else if (status === "checked") {
-      return "unchecked";
-    } else return status;
   };
 
-  const toggleComplete = (taskId: number, status: string) => {
-    let response;
-
-      fetchedTask.map((task: any) => {
-        if (task.id === taskId) {
-          return {
-            ...task,
-            status: getStatus(status),
-            animate: true,
-          };
-        }
-        return task;
-      });
+  const toggleCheckbox = async (taskId: string) => {
+    await addToggleCheck(taskId);
   };
+
+  const deleteTask = async (taskId: string) => {
+    await addDeleteTask(taskId)
+  }
 
   return (
-    <div className="w-1/2 p-3">
-      <div className="mt-2 mb-10">
-        <h1 className="text-4xl font-semibold text-white">To do List</h1>
+    <div className="w-1/2 px-3 flex flex-col justify-around relative">
+      <div className="h-4/5 overflow-auto">
+        <div className="mt-2 mb-5 top-0 sticky z-20">
+          <h1 className="text-4xl font-semibold text-white">To do Tasks</h1>
+        </div>
+        {fetchedUser &&
+          todoTask.map((task: Task) => (
+            <TaskItem
+              key={task.id}
+              data={task}
+              toggleCheckbox={toggleCheckbox}
+              isEditable={isEditable}
+              // setUpdateTitle={setUpdateTitle}
+              deleteTask={deleteTask}
+            />
+          ))}
       </div>
-      {fetchedTask.map((task: Record<string, any>) => (
-        <TaskItem key={task.id} allData={task} toggleComplete={toggleComplete} />
-      ))}
-      <Input
-        type="text"
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }}
-        value={title}
-      />
-      <Button label="Add task" onClick={onClick} disabled={false} />
+      <div className="pb-3 mb-4 h-1/5">
+        <Input
+          type="text"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={title}
+        />
+        <Button label="Add task" onClick={addTask} disabled={false} />
+      </div>
     </div>
   );
 };

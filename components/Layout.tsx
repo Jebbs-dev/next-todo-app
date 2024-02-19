391081417d75436a54d4ec2a1f63dfac611aae71
Jebbs-dev <ojebiyifulness@gmail.com>
2024-02-19 19:18:31 +0100

    style: set minimum height

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,11 +11,10 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <Nav />
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-80 fixed max-h-[70vh] overflow-auto left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 mx-auto w-full max-w-3xl rounded-2xl p-2 flex flex-row text-center">
+          <div className="bg-black bg-opacity-80 fixed min-h-[60vh] max-h-[70vh] overflow-auto left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 mx-auto w-full max-w-3xl rounded-2xl p-2 flex flex-row text-center">
             {children}
           </div>
         </div>
-        N
       </div>
     </div>
   );

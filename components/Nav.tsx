f605bd92053c131551f4db03476f7e785748e4c2
Jebbs-dev <ojebiyifulness@gmail.com>
2024-05-01 19:55:01 +0100

    fix: eslint error
    Change img to next Image component for optimisation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { signOut } from "next-auth/react";
 import { useUserQuery } from "@/queries/fetch-user";
+import Image from "next/image";
 
 const Nav = () => {
   const { data: fetchedUser } = useUserQuery();
 
   return (
     <nav className="px-12 py-5 flex justify-between items-center">
-      <img src="/vercel.svg" className="h-6" alt="Logo" />
+      <Image src="/vercel.svg" width={24} height={24} className="h-6" alt="Logo" />
       <h2 className="text-5xl text-white">Hello, {fetchedUser?.name ? fetchedUser?.name : `User`}</h2>
       <span
         onClick={() => signOut()}
